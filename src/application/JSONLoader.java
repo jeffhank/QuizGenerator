393c87a20bfef04c6c764209a7301833493dcc61
Aidan Kaiser <aidankaiser@Aidans-MacBook-Pro.local>
2019-04-30 17:33:56 -0500

    json loader started

---
+++ src/application/JSONLoader.java
@@ -0,0 +1,15 @@
+package application;
+
+import java.util.Hashtable;
+
+public class JSONLoader {
+  String jsonFilePath;
+  
+ // public JSONLoader(String jsonFilePath) {
+   // this.jsonFilePath = jsonFilePath;
+  //}
+  
+ // public Hashtable<Question> readJsonAndRetrieveData(String jsonFilePath) {
+    
+  //}
+}
