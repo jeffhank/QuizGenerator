9a9759ba342ebb32c1e3fbd04eef74d37dc2729d
brettk10 <bekessler2@wisc.edu>
2019-04-30 17:42:34 -0500

    JsonSaver class

--- src/application/QuestionPane.java
+++ src/application/QuestionPane.java
@@ -1,17 +1,20 @@
 package application;
 
 import javafx.scene.layout.BorderPane;
+import javafx.scene.text.Text;
 import javafx.stage.Stage;
 
 public class QuestionPane extends BorderPane {
 
-  private QuizApplication application;
+	private QuizApplication application;
 
-  public QuestionPane(QuizApplication application) {
-    this.application = application;
-  }
+	public QuestionPane(QuizApplication application) {
+		this.application = application;
+		setup();
+	}
 
-  public void setup(Stage primaryStage) {
-	  
-  }
+	private void setup() {
+		Text title = new Text("Question");
+		this.setTop(title);
+	}
 }
