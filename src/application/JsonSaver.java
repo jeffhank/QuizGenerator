d73495647a99ff043e37d46b821e60d2debbc140
brettk10 <bekessler2@wisc.edu>
2019-04-30 19:32:49 -0500

    JsonSaver

--- src/application/JsonSaver.java
+++ src/application/JsonSaver.java
@@ -1,7 +1,10 @@
 package application;
 
 import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.Hashtable;
 import java.util.Set;
 
@@ -9,14 +12,14 @@ import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
 
 public class JsonSaver {
-	private Hashtable<String, ArrayList<Question>> hashFunction;
+	private HashMap<String, ArrayList<Question>> hashFunction;
 
-	public JsonSaver(Hashtable<String, ArrayList<Question>> hashFunction) {
+	public JsonSaver(HashMap<String, ArrayList<Question>> hashFunction) {
 		this.hashFunction = hashFunction;
 	}
 
-	public void saveQuestionSetJson(String jsonFilePath) {
-		File file = new File(jsonFilePath);
+	@SuppressWarnings({ "unchecked", "rawtypes" })
+	public void saveQuestionSetJson(String jsonFilePath) throws IOException {
 		JSONObject curr = new JSONObject();
 		Set keys = hashFunction.keySet();
 		String[] keyArray = (String[]) keys.toArray();
@@ -26,8 +29,24 @@ public class JsonSaver {
 				curr.put("questionText", hashFunction.get(keyArray[i]).get(j).getQuestionText());
 				curr.put("topic", hashFunction.get(keyArray[i]).get(j).getTopic());
 				curr.put("image", hashFunction.get(keyArray[i]).get(j).getImage());
-				curr.put("choiceArray", hashFunction.get(keyArray[i]).get(j).getChoiceArray());
+				ArrayList choiceArray = hashFunction.get(keyArray[i]).get(j).getChoiceArray();
+				JSONArray temp = new JSONArray();
+				for (int k = 0; k < choiceArray.size(); k++) {
+					JSONObject choiceArrayObject = new JSONObject();
+					if (hashFunction.get(keyArray[i]).get(j).getChoiceArray().get(k).getIsCorrect()) {
+						choiceArrayObject.put("isCorrect", "T");
+					} else {
+						choiceArrayObject.put("isCorrect", "F");
+					}
+					choiceArrayObject.put("choice",
+							hashFunction.get(keyArray[i]).get(j).getChoiceArray().get(k).getChoice());
+					temp.add(choiceArrayObject);
+				}
+				curr.put("choiceArray", temp);
 			}
+		}
+		try (FileWriter file = new FileWriter(jsonFilePath)) {
+			file.write(curr.toJSONString());
 		}
 	}
 }
