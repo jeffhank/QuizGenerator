f41b6f873471341081aa467ba0b2921baf5bd229
hankj <jhank@wisc.edu>
2019-05-02 14:01:26 -0500

    newQuestionPane comments

--- src/application/NewQuestionPane.java
+++ src/application/NewQuestionPane.java
@@ -16,6 +16,13 @@ import javafx.scene.control.ToggleGroup;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.GridPane;
 
+/**
+ * 
+ * This class represents the screen that appears when a new question is added by the user.
+ * Contains a text field to enter a new question. Also prompts the user for the number of 
+ * answer choices for that quesiton as well as the topic for it.
+ *
+ */
 public class NewQuestionPane extends BorderPane implements QScene {
 
 	private QuizApplication application;
@@ -24,29 +31,45 @@ public class NewQuestionPane extends BorderPane implements QScene {
 	private String topicString = "";
 	private GridPane grid = new GridPane();
 	private ArrayList<Answer> answerList = new ArrayList<Answer>();
-  private int correctRow = -1;
-
+    private int correctRow = -1;
+    
+    /**
+     * 
+     * constructor that initializes the application and calls setupLayout()
+     * @param QuizApplication to be used for this window screen
+     *
+     */
 	public NewQuestionPane(QuizApplication application) {
 		this.application = application;
 		setupLayout();
 	}
-
+	
+	/**
+	 * 
+	 * sets the style for the screen
+	 */
 	private void setupLayout() {
     this.setStyle("-fx-background-color: #FFFFFF;");
 	  firstQuestions();
 	}
 	
+	/**
+	 * Prompts user for the new question information. Calls askForAnswerChoices() to creates a 
+	 * question with its mutiple answer choices.
+	 */
 	private void firstQuestions() {
 	    numChoices = -1;
 	    answerList = new ArrayList<Answer>();
 	    grid.getChildren().clear();
 	    this.getChildren().clear();
+	    //creates title label and positions it in the center with 30 pt arial font
 	    Label titleLabel = new Label("Add A New Question");
 	    titleLabel.setPadding(new Insets(12));
 	    titleLabel.setStyle("-fx-font: 30 arial;");
 	    this.setTop(titleLabel);
 	    BorderPane.setAlignment(titleLabel, Pos.CENTER);
-
+	    
+	    //creates a text field so the user can input the number of answer choices they want 
 	    TextField question = new TextField();
 	    TextField numAnswerChoices = new TextField();
 	    TextField topic = new TextField();
@@ -72,14 +95,15 @@ public class NewQuestionPane extends BorderPane implements QScene {
 	          numChoices = Integer.parseInt(numAnswerChoices.getText());
 	          if(numChoices > 1 && numChoices < 6)
 	            askForAnswerChoices();
-	          else {
+	          else { //sends alert if user enters too little or too many answer choices
 	            Alert alert = new Alert(AlertType.INFORMATION);
 	            alert.setTitle("Invalid Input");
 	            alert.setContentText("Please ensure the number of choices is between 2 and 5.");
 	            alert.show();
 	          }
 	        }
-	        catch(NumberFormatException nfe) {
+	      //catches error thrown from parsing the entered text if user does not enter a number
+	        catch(NumberFormatException nfe) { 
 	          Alert alert = new Alert(AlertType.INFORMATION);
 	          alert.setTitle("Not a Number");
 	          alert.setContentText("Please ensure you enter a number in the number of choices field.");
@@ -87,7 +111,7 @@ public class NewQuestionPane extends BorderPane implements QScene {
 	        }
 	        topicString = topic.getText();
 	      }
-	      else {
+	      else { //alerts if user leaves any text fields blank when creating a new question
           Alert alert = new Alert(AlertType.INFORMATION);
           alert.setTitle("Error");
           alert.setContentText("All fields must be filled.");
@@ -99,6 +123,11 @@ public class NewQuestionPane extends BorderPane implements QScene {
 
 	}
 	
+	/**
+	 * prompts user to enter their answer choices and creates an answer list. Uses that answer
+	 * list to create the new question. Implements a back button and a submit button to finalize
+	 * the question.
+	 */
 	public void askForAnswerChoices() {
 	  grid.getChildren().clear();
 	  this.getChildren().clear();
@@ -110,7 +139,7 @@ public class NewQuestionPane extends BorderPane implements QScene {
 	  this.setTop(instruction);
 	  this.setAlignment(instruction, Pos.TOP_CENTER);
 	  ToggleGroup answerGroup = new ToggleGroup();
-	  for(int i=0; i<numChoices; i++) {
+	  for(int i=0; i<numChoices; i++) { //creates multiple choice buttons for each answer choice
 	    grid.add(new TextField(), 0, i);
 	    int currentRow = i;
 	    RadioButton choiceButton = new RadioButton();
@@ -123,12 +152,14 @@ public class NewQuestionPane extends BorderPane implements QScene {
 	  }
 	  this.setCenter(grid);
 	  
+	  //creates a back button that takes user to previous screen prompting for question info
 	  Button backButton = new Button("Back");
 	  backButton.setPrefWidth(200.0);
 	  submitButton.setPrefWidth(200.0);
 	  this.setLeft(backButton);
 	  this.setRight(submitButton);
 	  backButton.setOnAction(a -> firstQuestions());
+	  //creates the new question
 	  Question newQuestion = new Question("unused", questionString, topicString, "none", answerList);
 	  submitButton.setOnAction(a -> {
 	    for(int i=0; i<grid.getChildren().size(); i++) {
@@ -156,10 +187,10 @@ public class NewQuestionPane extends BorderPane implements QScene {
 	        }
 	      }
 	    }
-	    application.addToQuestionDb(topicString, newQuestion);
+	    application.addToQuestionDb(topicString, newQuestion); //adds new question to hash table
 	    application.getStartScene().updateTopicList();
 	    firstQuestions();
-	    application.switchScreen(AppScreen.START_SCREEN);
+	    application.switchScreen(AppScreen.START_SCREEN);//takes user back to start screen when done
 	  });
 	  this.setAlignment(backButton, Pos.BOTTOM_LEFT);
 	  this.setAlignment(submitButton, Pos.BOTTOM_RIGHT);
