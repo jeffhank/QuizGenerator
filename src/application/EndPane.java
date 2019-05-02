0806dcf62f6416b267642184c372ee95cd420007
hankj <jhank@wisc.edu>
2019-05-02 13:14:09 -0500

    EndPane comments

--- src/application/EndPane.java
+++ src/application/EndPane.java
@@ -11,16 +11,28 @@ import javafx.scene.control.ButtonType;
 import javafx.scene.control.Label;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.GridPane;
-
+/**
+ * 
+ * This class represents the end window of the quiz. After all questions are answered the user
+ * is taken to this screen to save and exit, exit without saving, or restart the quiz. Also 
+ * displays the final score for the user. 
+ *
+ */
 public class EndPane extends BorderPane implements QScene {
 
 	private QuizApplication application;
-
+	/**
+	 * constructor for the end pane that initializes the QuizApplication and calls setupLayout()
+	 * @param QuizApplication to be used for this screen
+	 */
 	public EndPane(QuizApplication application) {
 		this.application = application;
 		setupLayout();
 	}
-
+	
+	/**
+     * sets up the layout for the end window screen 
+     */
 	private void setupLayout() {
 
 		GridPane buttonGrid = new GridPane();
@@ -35,14 +47,16 @@ public class EndPane extends BorderPane implements QScene {
 		Alert alert1 = new Alert(AlertType.INFORMATION);
 		alert1.setTitle("Exiting without saving");
 		alert1.setContentText("You are exiting without saving. Thanks for taking the quiz. Goodbye!");
-		exitWithoutSaveButton.setOnAction(a -> {
+		exitWithoutSaveButton.setOnAction(a -> { //shows alert pop up when user exits without save
 			Optional<ButtonType> result = alert1.showAndWait();
 			if (result.get() == ButtonType.OK) {
 				System.exit(0);
 			}
 		});
-
+		
+		//switches screen to start pane when the restart quiz button is pressed
 		restartQuizButton.setOnAction(a -> application.switchScreen(AppScreen.START_SCREEN));
+		//switches screen to save screen when the user tries to save
 		saveAndExitButton.setOnAction(a -> application.switchScreen(AppScreen.SAVE_SCREEN));
 		buttonGrid.add(restartQuizButton, 0, 0);
 		buttonGrid.add(exitWithoutSaveButton, 1, 0);
@@ -52,6 +66,10 @@ public class EndPane extends BorderPane implements QScene {
 	}
 
   @Override
+  /**
+   * displays the final score label when this end pane is shown during the application. Also
+   * sets the style for the score label
+   */
   public void onShown() {
     Label titleLabel = new Label(
         "Final Score: " + application.getQuestionsCorrect() + "/" + application.getTotalQuestions() + " ("
