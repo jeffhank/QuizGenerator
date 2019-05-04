ab3008b7e89a366ec60c764a903a21edd516255a
hankj <jhank@wisc.edu>
2019-05-03 21:33:14 -0500

    qscene javadoc

--- src/application/QScene.java
+++ src/application/QScene.java
@@ -1,5 +1,10 @@
 package application;
 
+/**
+ * interface that contains the onShown() method that is implemented by window pane classes.
+ * This method dictates what each pane will do when shown.
+ *
+ */
 public interface QScene {
   void onShown();
 }
